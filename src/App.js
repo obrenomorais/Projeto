86187061a1f4bf7eb54373e6505ef63779d18390
obrenomorais <brenomorais834@gmail.com>
2023-05-22 18:47:57 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
-
+import { useState } from "react"; 
 
 function App() {
-  return <div className="App">sss</div>
+
+  const [Name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const displayInfo= () =>{
+    console.log(Name, age)
+  }
+
+
+  return <div className="App">
+    <div className='Conteiner'>
+    <label>Name</label>
+    <input type="text" 
+    onChange={(event) => {
+      setName(event.target.value);
+      }}
+      />
+    <label>Age</label>
+    <input type='number'
+     onChange={(event) => {
+      setAge(event.target.value);
+      }}
+      />
+    <button onClick={displayInfo}>Enviar</button>
+  </div>
+  </div>
+
 }
 
 export default App;
