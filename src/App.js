f6fc087cae1b4f51ff6fdf4258661ebc8b2e179a
obrenomorais <brenomorais834@gmail.com>
2023-05-23 22:45:46 -0300

    aham

--- src/App.js
+++ src/App.js
@@ -1,61 +1,85 @@
-import './App.css';
-import { useState } from "react"; 
-import Axios from 'axios';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
-
   const [Name, setName] = useState("");
   const [age, setAge] = useState(0);
 
+  const [id, setId] = useState(0);
+
   const [pessoas, setPessoas] = useState([]);
 
-
-
-  const displayInfo= () =>{
-    console.log(Name, age)
-  }
-
-  const addPessoa = () =>{
-    Axios.post('http://localhost:3001/create', {
-      name: Name, 
-      age: age
-    }).then(() => {
-    console.log("succeess");
-  });
+  const displayInfo = () => {
+    console.log(Name, age);
   };
 
+  const displayPessoa = () => {
+    console.log(pessoas);
+  };
 
-  const getPessoas = () =>{
-Axios.get('http://localhost:3001/pessoas'). then ( (response => {
-  console.log(response)
-})
+  const addPessoa = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: Name,
+      age: age,
+    }).then(() => {
+      console.log("succeess");
+    });
+  };
 
-    
-  )}
+  const getPessoas = () => {
+    Axios.get("http://localhost:3001/pessoas").then((response) => {
+      setPessoas(response.data);
+    });
+  };
 
-  return <div className="App">
-    <div className='Conteiner'>
-    <label>Name</label>
-    <input type="text" 
-    onChange={(event) => {
-      setName(event.target.value);
-      }}
-      />
-    <label>Age</label>
-    <input type='number'
-     onChange={(event) => {
-      setAge(event.target.value);
-      }}
-      />
-    <button onClick={addPessoa}>Enviar</button>
-  </div>
+  const deletePessoa = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      console.log("person deleted");
+      getPessoas();
+    });
+  };
+  return (
+    <div className="App">
+      <div className="Conteiner">
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <button className="botaoteste" onClick={addPessoa}>
+          Enviar
+        </button>
 
-      <div className='Pessoas'> 
-      <button onClick={getPessoas}>Mostrar Pessoas</button> 
-      
+        <button onClick={getPessoas}>Mostrar Pessoas</button>
       </div>
-  </div>
 
+      <div className="Pessoas">
+        {pessoas &&
+          pessoas.map((val, key) => {
+            return (
+              <div className="pessoa" key={val.id}>
+                <button onClick={() => deletePessoa(val.id)}>Deletar</button>
+
+                <div className="quadrado">
+                  <h3>{val.Name}</h3>
+                  <h3>{val.Age}</h3>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
